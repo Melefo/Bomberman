341d51805092cfba23216243e36902e47b765860
ArthurC <arthur.croix@epitech.eu>
2021-05-28 09:42:41 +0200

    XMLGenerator base

---
+++ XMLGenerator/sources/XMLGenerator.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-NAN-4-1-indiestudio-victor.trencic
+** File description:
+** XMLGenerator
+*/
+
+#ifndef XMLGENERATOR_HPP_
+#define XMLGENERATOR_HPP_
+
+class XMLGenerator {
+    public:
+        XMLGenerator();
+        ~XMLGenerator();
+
+    protected:
+    private:
+};
+
+#endif /* !XMLGENERATOR_HPP_ */
