2079f1ed6f3cbffddccc451a8602696b034c3a4b
arthurCroix <arthur.croix@epitech.eu>
2021-06-04 16:40:06 +0200

    Adding tests for XMLGeneration

--- XMLGenerator/tests/testXMLGenerator.cpp
+++ XMLGenerator/tests/testXMLGenerator.cpp
@@ -11,43 +11,137 @@
 #include <criterion/redirect.h>
 #include <cstdio>
 
+Test(addTag, basic)
+{
+    std::string filepath = "test1createMap.xml";
+    TestXMLGenerator xmlFile(filepath);
+    std::string output;
+    std::string desiredOutput =
+        std::string("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")+
+        std::string("<Soupe>\n")+
+        std::string("\t<Ingredients>\n")+
+        std::string("\t\t<Carotte>\n")+
+        std::string("\t\t</Carotte>\n")+
+        std::string("\t</Ingredients>\n")+
+        std::string("</Soupe>\n");
+    xmlFile.addTag("Soupe");
+    xmlFile.addTag("Ingredients");
+    xmlFile.addTag("Carotte");
+    xmlFile.closeFile();
+    std::ifstream stream(filepath);
+
+    output = std::string((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+    cr_expect_eq(output, desiredOutput);
+    stream.close();
+    cr_assert_eq(remove(filepath.c_str()), 0);
+}
+
+Test(closeAndReopen, basic)
+{
+    std::string filepath = "test2createMap.xml";
+    TestXMLGenerator xmlFile(filepath);
+    std::string output;
+    std::string desiredOutput =
+        std::string("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")+
+        std::string("<Soupe>\n")+
+        std::string("\t<Ingredients>\n")+
+        std::string("\t\t<Carotte>\n")+
+        std::string("\t\t</Carotte>\n")+
+        std::string("\t</Ingredients>\n")+
+        std::string("\t<Ingredients>\n")+
+        std::string("\t\t<Patate>\n")+
+        std::string("\t\t</Patate>\n")+
+        std::string("\t</Ingredients>\n")+
+        std::string("</Soupe>\n");
+    xmlFile.addTag("Soupe");
+    xmlFile.addTag("Ingredients");
+    xmlFile.addTag("Carotte");
+    xmlFile.closeLastTag();
+    xmlFile.closeAndReopen("Ingredients");
+    xmlFile.addTag("Patate");
+    xmlFile.closeAndReopen("Ravioli");
+    xmlFile.closeFile();
+    std::ifstream stream(filepath);
+
+    output = std::string((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+    cr_expect_eq(output, desiredOutput);
+    stream.close();
+    cr_assert_eq(remove(filepath.c_str()), 0);
+}
+
+Test(closeLastTag, basic)
+{
+    std::string filepath = "test3createMap.xml";
+    TestXMLGenerator xmlFile(filepath);
+    std::string output;
+    std::string desiredOutput =
+        std::string("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")+
+        std::string("<Soupe>\n")+
+        std::string("\t<Ingredients>\n")+
+        std::string("\t</Ingredients>\n")+
+        std::string("\t<Carotte>\n")+
+        std::string("\t</Carotte>\n")+
+        std::string("</Soupe>\n");
+    xmlFile.addTag("Soupe");
+    xmlFile.addTag("Ingredients");
+    xmlFile.closeLastTag();
+    xmlFile.addTag("Carotte");
+    xmlFile.closeFile();
+    std::ifstream stream(filepath);
+
+    output = std::string((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+    cr_expect_eq(output, desiredOutput);
+    stream.close();
+    cr_assert_eq(remove(filepath.c_str()), 0);
+}
+
+Test(closeFile, basic)
+{
+    std::string filepath = "test4createMap.xml";
+    TestXMLGenerator xmlFile(filepath);
+    std::string output;
+    std::string desiredOutput =
+        std::string("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")+
+        std::string("<Soupe>\n")+
+        std::string("\t<Ingredients>\n")+
+        std::string("\t\t<Carotte>\n")+
+        std::string("\t\t\t<Orange>\n")+
+        std::string("\t\t\t</Orange>\n")+
+        std::string("\t\t</Carotte>\n")+
+        std::string("\t</Ingredients>\n")+
+        std::string("</Soupe>\n");
+    xmlFile.addTag("Soupe");
+    xmlFile.addTag("Ingredients");
+    xmlFile.addTag("Carotte");
+    xmlFile.addTag("Orange");
+    xmlFile.closeFile();
+    std::ifstream stream(filepath);
+
+    output = std::string((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+    cr_expect_eq(output, desiredOutput);
+    stream.close();
+    cr_assert_eq(remove(filepath.c_str()), 0);
+}
+
 Test(write, basic)
 {
-    std::vector<std::string> empty;
-    std::string filepath = "test1write.xml";
-    TestXMLGenerator xmlFile(empty, filepath);
+    std::string filepath = "test5createMap.xml";
+    TestXMLGenerator xmlFile(filepath);
     std::string output;
     std::string desiredOutput =
         std::string("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")+
-        std::string("<Map>\n")+
-        std::string("<OPERATOR TYPE=\"IN\"/>\n<OPERAND>\n\t<LIST>\n")+
-        std::string("\t<LIST>\n</OPERAND>\n")+
-        std::string("</Map>\n");
-    std::ifstream stream(filepath);
-
-    output = std::string((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
-    cr_expect_eq(output, desiredOutput);
-    stream.close();
-    xmlFile.write(std::string("This is a test."));
-    stream.open(filepath);
-    output = std::string((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
-    stream.close();
-    cr_expect_eq(output, desiredOutput+std::string("This is a test."));
-    cr_assert_eq(remove(filepath.c_str()), 0);
-}
-
-Test(createMap, empty)
-{
-    std::vector<std::string> empty;
-    std::string filepath = "test2createMap.xml";
-    TestXMLGenerator xmlFile(empty, filepath);
-    std::string output;
-    std::string desiredOutput =
-        std::string("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")+
-        std::string("<Map>\n")+
-        std::string("<OPERATOR TYPE=\"IN\"/>\n<OPERAND>\n\t<LIST>\n")+
-        std::string("\t<LIST>\n</OPERAND>\n")+
-        std::string("</Map>\n");
+        std::string("<Soupe>\n")+
+        std::string("\t<Ingredients>\n")+
+        std::string("\t\t<Carotte>\n")+
+        std::string("Vert")+
+        std::string("\t\t</Carotte>\n")+
+        std::string("\t</Ingredients>\n")+
+        std::string("</Soupe>\n");
+    xmlFile.addTag("Soupe");
+    xmlFile.addTag("Ingredients");
+    xmlFile.addTag("Carotte");
+    xmlFile.write("Vert");
+    xmlFile.closeFile();
     std::ifstream stream(filepath);
 
     output = std::string((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
@@ -56,25 +150,26 @@ Test(createMap, empty)
     cr_assert_eq(remove(filepath.c_str()), 0);
 }
 
-Test(createMap, not_empty)
+Test(addValue, basic)
 {
-    std::vector<std::string> full = {
-        "Hello",
-        "World",
-        "!"
-    };
-    std::string filepath = "test3createMap.xml";
-    TestXMLGenerator xmlFile(full, filepath);
+    std::string filepath = "test6createMap.xml";
+    TestXMLGenerator xmlFile(filepath);
     std::string output;
     std::string desiredOutput =
         std::string("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")+
-        std::string("<Map>\n")+
-        std::string("<OPERATOR TYPE=\"IN\"/>\n<OPERAND>\n\t<LIST>\n")+
-        std::string("\t\t<LITERAL VALUE=\"Hello\"/>\n")+
-        std::string("\t\t<LITERAL VALUE=\"World\"/>\n")+
-        std::string("\t\t<LITERAL VALUE=\"!\"/>\n")+
-        std::string("\t<LIST>\n</OPERAND>\n")+
-        std::string("</Map>\n");
+        std::string("<Soupe>\n")+
+        std::string("\t<Ingredients>\n")+
+        std::string("\t\t<Carotte>1</Carotte>\n")+
+        std::string("\t\t<Poulet>1</Poulet>\n")+
+        std::string("\t\t<Patate>2</Patate>\n")+
+        std::string("\t</Ingredients>\n")+
+        std::string("</Soupe>\n");
+    xmlFile.addTag("Soupe");
+    xmlFile.addTag("Ingredients");
+    xmlFile.addValue("Carotte", 1);
+    xmlFile.addValue("Poulet", 1);
+    xmlFile.addValue("Patate", 2);
+    xmlFile.closeFile();
     std::ifstream stream(filepath);
 
     output = std::string((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
