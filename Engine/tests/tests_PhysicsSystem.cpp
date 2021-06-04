83a2e9170a3c9d99843425141fdf86369ec7d609
Gabriel <gabriel.griffin@epitech.eu>
2021-06-04 11:12:24 +0200

    Adding tests, and overlapbox function in collisionsystem

---
+++ Engine/tests/tests_PhysicsSystem.cpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-NAN-4-1-indiestudio-victor.trencic
+** File description:
+** tests_PhysicsSystem
+*/
+
+#include <criterion/criterion.h>
+#include "PhysicsSystem.hpp"
+#include "Coordinator.hpp"
+#include "Transform.hpp"
+#include <iostream>
+
+Test(PhysicsSystem, velocity)
+{
+    std::unique_ptr<ECS::Coordinator>& coordinator = ECS::Coordinator::GetInstance();
+    Component::PhysicsSystem& physicsSystem = coordinator->AddSystem<Component::PhysicsSystem>();
+
+    ECS::Entity &entity = coordinator->CreateEntity();
+    entity.AddComponent<Component::Transform>(RayLib::Vector3(5.0f, 0.0f, 0.0f));
+    entity.AddComponent<Component::PhysicsBody>();
+
+    Component::PhysicsBody& physicsBody = entity.GetComponent<Component::PhysicsBody>();
+    Component::Transform& transform = entity.GetComponent<Component::Transform>();
+    physicsBody.velocity = RayLib::Vector3(1.0f, 0.0f, 0.0f);
+
+    physicsSystem.FixedUpdate(entity);
+
+    // position + velocity
+    std::cout << "Pos X " << transform.position.x << std::endl;
+    cr_assert_eq(transform.position.x, 6.0f);
+}
