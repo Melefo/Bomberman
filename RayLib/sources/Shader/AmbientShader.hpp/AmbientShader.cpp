3037ae9cee43b0842a4e143a17235ff60ef50ab5
arthurCroix <arthur.croix@epitech.eu>
2021-06-14 15:25:29 +0200

    Added a new usable shader type. Linked with ambient_lighting.fs

---
+++ RayLib/sources/Shader/AmbientShader.hpp/AmbientShader.cpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-NAN-4-1-indiestudio-victor.trencic
+** File description:
+** AmbientShader
+*/
+
+#include "AmbientShader.hpp"
+#include <iostream>
+
+AmbientShader::AmbientShader(RayLib::Color& color, const std::string& ShaderPath)
+    : RayLib::Shader("", ShaderPath+"ambient_lighting.fs")
+{
+    int location = GetLocation("color");
+    float shaderColor[4] = {
+        color.r/static_cast<float>(255),
+        color.g/static_cast<float>(255),
+        color.b/static_cast<float>(255),
+        color.a/static_cast<float>(255)};
+
+    SetValue(location, shaderColor, SHADER_UNIFORM_VEC4);
+}
