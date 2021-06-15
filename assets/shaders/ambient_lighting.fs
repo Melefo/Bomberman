ac2a0e7eb0d934d8fc4a859cc03ab89b90e73124
arthurCroix <arthur.croix@epitech.eu>
2021-06-15 15:13:43 +0200

    Added informative commentaries on input vectors

--- assets/shaders/ambient_lighting.fs
+++ assets/shaders/ambient_lighting.fs
@@ -1,7 +1,7 @@
 #version 330
 
-in vec2 fragTexCoord;
-in vec4 fragColor;
+in vec2 fragTexCoord; // Position of the fs texel in the screen (0.0 - 1.0)
+in vec4 fragColor; // The color of the basic screen (without anything)
 
 uniform sampler2D texture0;
 uniform vec4 colDiffuse;
