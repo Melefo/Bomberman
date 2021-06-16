98e513f61a3756656f1f7ac44f646e06c6eb978a
arthurCroix <arthur.croix@epitech.eu>
2021-06-16 17:50:35 +0200

    Added drawable main

--- Prototype/sources/Drawable/main.cpp
+++ Prototype/sources/Drawable/main.cpp
@@ -6,10 +6,27 @@
 */
 
 #include "Coordinator.hpp"
+#include "Camera.hpp"
+#include "Window.hpp"
 #include <iostream>
 
 int main(void)
 {
-    std::cout << "Drawable proto" << std::endl;
+    std::unique_ptr<ECS::Coordinator>& coordinator = ECS::Coordinator::GetInstance();
+
+    RayLib::Camera3D camera = RayLib::Camera3D(RayLib::Vector3(0.0f, 10.0f, 10.0f), RayLib::Vector3(), RayLib::Vector3(0.0f, 1.0f, 0.0f));
+    std::unique_ptr<RayLib::Window>& window = RayLib::Window::GetInstance(RayLib::Vector2<int>(1920, 1080), "Bomberman");
+
+    window->SetTargetFPS(60);
+    window->SetExitKey(KEY_ESCAPE);
+    while (!window->WindowShouldClose() && !coordinator->CloseWindow)
+    {
+        window->BeginDrawing();
+        window->ClearBackground(RAYWHITE);
+        camera.BeginMode();
+        coordinator->Run();
+        camera.EndMode();
+        window->EndDrawing();
+    }
     return (0);
 }
