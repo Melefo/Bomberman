e8ff7775329850689b69f8ff603d7f0d2131c15e
Gabriel <gabriel.griffin@epitech.eu>
2021-06-16 17:27:45 +0200

    Adding drawable proto

---
+++ Prototype/sources/Drawable/main.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-NAN-4-1-indiestudio-victor.trencic
+** File description:
+** main
+*/
+
+#include "Coordinator.hpp"
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Drawable proto" << std::endl;
+    return (0);
+}
