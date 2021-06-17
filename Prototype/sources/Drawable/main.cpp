bf7cce4d6fa31484ceaf66ed8da24a687506da95
Gabriel <gabriel.griffin@epitech.eu>
2021-06-17 10:55:30 +0200

    Added shader example

--- Prototype/sources/Drawable/main.cpp
+++ Prototype/sources/Drawable/main.cpp
@@ -21,38 +21,35 @@
 int main(void)
 {
     std::unique_ptr<ECS::Coordinator>& coordinator = ECS::Coordinator::GetInstance();
-    RayLib::Camera3D camera = RayLib::Camera3D(RayLib::Vector3(0.0f, 50.0f, 0.0f), RayLib::Vector3(), RayLib::Vector3(0.0f, 0.0f, 0.0f));
+    RayLib::Camera3D camera = RayLib::Camera3D(RayLib::Vector3(0.0f, 10.0f, 0.0f), RayLib::Vector3(), RayLib::Vector3(0.0f, 1.0f, 0.0f));
     std::unique_ptr<RayLib::Window>& window = RayLib::Window::GetInstance(RayLib::Vector2<int>(1920, 1080), "Bomberman");
 
     coordinator->AddSystem<Component::RenderSystem>();
 
-    //Component::ButtonCallbacks::GenerateBackgroundMap();
+    // sphere mesh
+    RayLib::Mesh sphereMesh(1.0f, 20, 20);
+    // drawable
+    ECS::Entity& sphereEntity = coordinator->CreateEntity();
+    sphereEntity.AddComponent<Component::Transform>();
+    sphereEntity.AddComponent<Component::Drawable3D>(sphereMesh);
+    Component::Drawable3D& drawable = sphereEntity.GetComponent<Component::Drawable3D>();
 
-    EntityFactory factory(*coordinator.get(), camera);
-
-    for (int i = 0; i < 100; i++) {
-
-        RayLib::Shader& shader = AssetCache::GetAsset<RayLib::Shader>("../assets/shaders/mask");
-        RayLib::Texture& text = AssetCache::GetAsset<RayLib::Texture>("../assets/Box/Box_texture.png");
-
-        ECS::Entity& box = factory.createBox(0, false);
-        box.GetComponent<Component::Drawable3D>().SetMaterialTexture(0, MATERIAL_MAP_DIFFUSE, text);
-        //box.GetComponent<Component::Drawable3D>().SetMaterialShader(0, shader);
-    }
+    RayLib::Shader& shader = AssetCache::GetAsset<RayLib::Shader>("../assets/shaders/mask");
+    drawable.SetMaterialShader(0, shader);
 
     window->SetTargetFPS(60);
     window->SetExitKey(KEY_ESCAPE);
+    camera.SetCameraMode(CAMERA_FREE);
     while (!window->WindowShouldClose() && !coordinator->CloseWindow)
     {
         camera.Update();
-
         window->BeginDrawing();
         window->ClearBackground(RAYWHITE);
         camera.BeginMode();
 
         coordinator->Run();
 
-        window->DrawGrid(100.0f, 1.0f);
+        window->DrawGrid(10, 1.0f);
 
         camera.EndMode();
         window->EndDrawing();
